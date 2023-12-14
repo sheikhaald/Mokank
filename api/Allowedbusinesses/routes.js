d7d928b578a228f042568a82557a81caa243cee4
Ibrahim <ebrahimmsafar@gmail.com>
2023-12-14 23:15:07 +0300

    fix

--- api/Allowedbusinesses/routes.js
+++ api/Allowedbusinesses/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const passport = require("passport");
 const {
   updateAllowedBusiness,
@@ -18,28 +17,5 @@ router.param("BusinessId", async (req, res, next, BusinessId) => {
 router.post("/create", createAllowedBusiness);
 router.get("/get", getAllAllowedBusinesss);
 router.put("/:BusinessId", updateAllowedBusiness);
-=======
-
-const {
-  findAllowedBusiness,
-  createAllowedBusiness,
-  getAllAllowedBusinesses,
-  updateAllowedBusiness,
-  getOneAllowedBusiness,
-  deleteAllowedBusiness,
-} = require("./controllers");
-
-router.param("AllowedBusinessId", async (req, res, next, AllowedBusinessId) => {
-  const allowedBusiness = await findAllowedBusiness(AllowedBusinessId, next);
-  req.allowedBusiness = allowedBusiness;
-  next();
-});
-
-router.post("/", createAllowedBusiness);
-router.get("/", getAllAllowedBusinesses);
-router.put("/:AllowedBusinessId", updateAllowedBusiness);
-router.delete("/:AllowedBusinessId", deleteAllowedBusiness);
-router.get("/:AllowedBusinessId", getOneAllowedBusiness);
->>>>>>> origin/main
 
 module.exports = router;
