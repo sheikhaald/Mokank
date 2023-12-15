7fed40cb979958e9a1d9a94d9b9b11ee9cf6a757
rawanalmukhled <rawanalmekhled@gmail.com>
2023-12-15 16:05:17 +0300

    working

--- models/Place.js
+++ models/Place.js
@@ -1,7 +1,14 @@
 const { model, Schema } = require("mongoose");
 
 const PlaceSchema = new Schema({
-  reviews: String,
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true, unique: true },
+  price: { type: String, required: true, unique: true },
+  image: { type: String, required: false, unique: true },
+  businessType: { type: String, required: false, unique: true },
+  allowedBusiness: { type: String, required: false, unique: true },
+  startDate: { type: String, required: false, unique: true },
+  endDate: { type: String, required: false, unique: true },
   BusinessUser: [{ type: Schema.Types.ObjectId, ref: "businessuser" }],
   user: [{ type: Schema.Types.ObjectId, ref: "user" }],
 });
